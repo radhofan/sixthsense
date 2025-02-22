37ff6e5514f919bbef9e732ac64c35581410c9f8
Radhofan <radhofanazizi@gmail.com>
2025-02-22 11:59:13 +0700

    ada

--- reproduce.sh
+++ reproduce.sh
@@ -30,7 +30,7 @@ tar -xzvf sixthsense/csvs/csvs.tar.gz -C sixthsense/csvs --strip-components=1
 
 # Run SixthSense
 python sixthsense/train.py -f sixthsense/csvs/lrm_features.csv -l sixthsense/csvs/lrm_metrics.csv -a rf -m rhat_min -suf avg -bw -plt -saveas sixthsense/plots/results_rhat_min_lrm.png -keep _ast_ dt_ var_min var_max data_size -st -tname lightspeed -cv -ignore_vi
-
+exit 0
 
 
 
